48a6ffbbb00491ec1a40a07a06668a06a71a5b63
Hussein Elmasri <helmasri88@gmail.com>
2026-09-18 15:47:17 +0000

    adjsut

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  // Replace 'catalogue-price-checker' with your actual repository name
-  base: '/catalogue-price-checker/',
+  base: '/CatalogueGitex99444015/',
 });
