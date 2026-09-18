90ab5f0d93e727946d3d5133ae5a449ad23f8581
Hussein Elmasri <helmasri88@gmail.com>
2026-09-18 15:41:07 +0000

    adjust

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  // Replace 'catalogue-price-checker' with your actual repository name
+  base: '/catalogue-price-checker/',
+});
